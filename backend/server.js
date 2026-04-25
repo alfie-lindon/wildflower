87cfbd62cfb1e9ab44adafc32856760701464250
Alfie Lindon S. Adrales <alfielindon.adrales@gmail.com>
2026-04-26 00:44:59 +0800

    Delete Express Backend

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-import app from './src/app.js'
-import './src/config/db.js'
-
-const PORT = process.env.PORT || 8000
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode`);
-});
