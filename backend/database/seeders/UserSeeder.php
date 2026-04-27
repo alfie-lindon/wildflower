8c95a95ad955f3203b9bc15170b39696a654228d
Alfie Lindon S. Adrales <alfielindon.adrales@gmail.com>
2026-04-28 01:21:30 +0800

    Admin Seeder

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +15,22 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::beginTransaction();
+        try{
+            User::create([
+                'name' => 'Admin',
+                'phone' => null,
+                'email' => 'admin@wildflower.com',
+                'password' => Hash::make('Admin123!'),
+                'is_admin' => true,
+                'email_verified_at' => now(),
+                'created_at' =>now(),
+                'updated_at' =>now(),
+            ]);
+        DB::commit();
+        }catch(\Throwable $e){
+            DB::rollBack();
+            throw $e;
+        }
     }
 }
